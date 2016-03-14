0a0b92777985ba04e33dc209c6ec6aad8d90aebc
Dawid Kuźmiński <d.kuzminski@outlook.com>
2016-03-14 13:52:14 +0100

    Added class EventModel on iCallWebApp Model

---
+++ iCalWebApp/Models/EventModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+
+namespace iCalWebApp.Models
+{
+    class EventModel : IEvent
+    {
+        public string Title { get; set; }
+        public string Comment { get; set; }
+
+        public IDateRange DateRange { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
