504821885218aa0150f374824ddb213e5a54c5a4
Dawid Kuźmiński <d.kuzminski@outlook.com>
2016-03-14 13:34:24 +0100

    Added model class Event

---
+++ Core/Events/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+
+namespace Core.Events
+{
+    /// <summary>
+    /// Model wydarzenia
+    /// </summary>
+    class Event : IEvent
+    {
+        public string Title { get; set; }
+        public string Comment { get; set; }
+
+        public IDateRange DateRange { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
