8764ec7058a8e9de570a633f9ead1b10032b1a82
turbulencje <anita.jedrusik@outlook.com>
2016-03-14 14:40:40 +0100

    added EventsListController

---
+++ iCalWebApp/Controllers/EventsListController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using iCalWebApp.Models;
+
+namespace iCalWebApp.Controllers
+{
+	
+	public class EventsListController : ApiController
+	{
+
+		private EventModel[] events = new EventModel[]
+		{
+			new EventModel { }
+		};
+
+		public IEnumerable<EventModel> GetAllEvents()
+		{
+			return events;
+		}
+	}
+}
