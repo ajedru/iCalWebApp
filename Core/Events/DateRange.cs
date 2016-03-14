ce559cf1972da4d6ed7d754d6a3d20745659980d
Dawid Kuźmiński <d.kuzminski@outlook.com>
2016-03-14 16:52:08 +0100

    Added DateRange and completed IDateRange

---
+++ Core/Events/DateRange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Core.Interfaces;
+
+namespace Core.Events
+{
+    public class DateRange : IDateRange
+    {
+
+        public DateRange( DateTime from, DateTime to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                TimeSpan difference = To - From;
+                return difference;
+            }
+        }
+    }
+}
