1fb14fe23cf8ef13eb8940d60c436c0308457686
turbulencje <anita.jedrusik@outlook.com>
2016-03-18 20:50:24 +0100

    All: Added Nugets project, changed testing framework to NUnit, changed tests to use NUnit

--- CoreTests/Events/DateRangeTests.cs
+++ CoreTests/Events/DateRangeTests.cs
@@ -1,27 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Core.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using NUnit.Framework;
 
 namespace Core.Events.Tests
 {
-    [TestClass]
-    public class DateRangeTests
-    {
-        [TestMethod]
-        public void DurationShouldReturnProperTimeSpan()
-        {
-            //Arrange
-            DateTime from = new DateTime(2000, 06, 10);
-            DateTime to = new DateTime(2000,06, 20);
-            var dateRange = new DateRange(from, to);
-            //Act
-            TimeSpan result = dateRange.Duration;
-            //Assert
-            Assert.AreEqual(new TimeSpan(10,0,0,0), result);
-        }
-    }
+	
+	[TestFixture]
+	public class DateRangeTests
+	{
+		[Test]
+		public void DurationShouldReturnProperTimeSpan()
+		{
+			//Arrange
+			DateTime from = new DateTime(2000, 06, 10);
+			DateTime to = new DateTime(2000,06, 20);
+			var dateRange = new DateRange(from, to);
+
+			//Act
+			TimeSpan result = dateRange.Duration;
+
+			//Assert
+			Assert.AreEqual(new TimeSpan(10,0,0,0), result);
+		}
+	}
 }
