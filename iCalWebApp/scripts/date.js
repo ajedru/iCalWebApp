e7689b8dcd050342977ade4c5c3a4579ab74b205
Alicja Wolnik <alicja.wolnik@outlook.com>
2016-03-21 11:35:14 +0100

    Web: Added datepicker

---
+++ iCalWebApp/scripts/date.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function(){
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+    date_input.datepicker({
+        format: 'dd/mm/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+        minDate: 0
+    })
+    debugger;
+})
+
+$(document).ready(function(){
+    var date_input=$('input[name="date1"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    date_input.datepicker({
+        format: 'dd/mm/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+        minDate: 0
+    })
+})
