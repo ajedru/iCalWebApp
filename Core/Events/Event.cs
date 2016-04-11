2d296dab96bbeedb82f466e740913288f9b1d58d
TPapaj <p4p4y@o2.pl>
2016-04-11 14:36:26 +0200

    Core : Added alarm to Event

--- Core/Events/Event.cs
+++ Core/Events/Event.cs
@@ -19,8 +19,17 @@ namespace Core.Events
 			this.guid = guid;
 		}
 
-		public string Title { get; set; }
+        public Event(string title, string comment, IDateRange dateRange, DateTime creationDate, Guid guid, int alarmTime)
+            : this(title, comment, dateRange, creationDate, guid)
+        { 
+            AlarmTime = alarmTime;
+        }
+        // Tomasz Papaj, konstruktor z alarmem
+
+
+        public string Title { get; set; }
 		public string Comment { get; set; }
+        public int AlarmTime { get; set; } // Tomasz Papaj, Alarm eventu w minutach
 
 		public IDateRange DateRange { get; set; }
 
@@ -30,5 +39,12 @@ namespace Core.Events
 		{
 			get { return guid; }
 		}
+
+        public Boolean HasAlarm()
+        {
+            if (AlarmTime > 0) return true;
+            else return false;
+        }
+        //Tomasz Papaj, metoda sprawdzajaca obecnosc alarmu
 	}
 }
--- Core/Interfaces/IEvent.cs
+++ Core/Interfaces/IEvent.cs
@@ -9,10 +9,12 @@ namespace Core.Interfaces
 	{
 		string Title { get; set; }
 		string Comment { get; set; }
+        int AlarmTime { get; set; } // Tomasz Papaj, Alarm eventu w minutach
 
-		IDateRange DateRange { get; set; }
+        IDateRange DateRange { get; set; }
 
 		DateTime CreationDate { get; set; }
 		Guid Guid { get; }
-	}
+        Boolean HasAlarm(); // Tomasz Papaj, metoda sprawdzajaca obecnosc alarmu
+    }
 }
