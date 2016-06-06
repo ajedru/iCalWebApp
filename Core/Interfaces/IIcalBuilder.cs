51053e3ed415b3fe132fa044e5c866addfa5e7bd
Tomasz Papaj <p4p4y@o2.pl>
2016-06-06 13:48:35 +0200

    Created Builder class.

---
+++ Core/Interfaces/IIcalBuilder.cs
@@ -0,0 +1,15 @@
+﻿using DDay.iCal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    interface IIcalBuilder
+    {
+        DDay.iCal.Event CreateEvent(string summary, string description, DateTime start, DateTime end, Alarm alarm);
+        void AddEventToCalendar(iCalendar calendar, DDay.iCal.Event cacheEvent);
+    }
+}
--- Core/Parsers/Parser.cs
+++ Core/Parsers/Parser.cs
@@ -34,38 +34,6 @@ namespace Core.Events
 			return output;
 		}
 
-		private Alarm CreateAlarm(int days, int hours, int minutes, int seconds)
-		{
-			if (days == 0 & hours == 0 & minutes == 0 & seconds == 0)
-				return null;
-			else
-			{
-				Alarm alarm = new Alarm();
-				alarm.Trigger = new Trigger(new TimeSpan(-days, -hours, -minutes, -seconds));
-				alarm.Action = AlarmAction.Display;
-				return alarm;
-			}
-		}
-
-		private void CreateAndAddEvent(string summary, string description, DateTime start, DateTime end, Alarm alarm)
-		{
-			DDay.iCal.Event evt = calendar.Create<DDay.iCal.Event>();
-			iCalDateTime today = iCalDateTime.Today;
-			evt.Start = DateToiCalDate(start);
-			evt.End = DateToiCalDate(end); // This also sets the duration
-			evt.Description = description;
-			evt.Summary = summary;
-			if (alarm != null) evt.Alarms.Add(alarm);
-		}
-
-		private IDateTime DateToiCalDate(DateTime date)
-		{
-			iCalDateTime today = iCalDateTime.Today;
-			return today.AddYears(date.Year - today.Year)
-				.AddMonths(date.Month - today.Month)
-				.AddDays(date.Day - today.Day)
-				.AddHours(date.Hour - today.Hour)
-				.AddMinutes(date.Minute - today.Minute);
-		}
+		
 	}
 }
